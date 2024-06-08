65ae3e7ed346bc85bf3cf5878c6bed30d6bfa50f
Derek Sliman <lionext.studio@gmail.com>
2024-06-08 14:30:02 +0300

    Remove entity pool

--- Scripts/Boot/ProjectData.cs
+++ Scripts/Boot/ProjectData.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using TinyMVC.Dependencies;
-using TinyMVC.Entities;
 
 #if ODIN_INSPECTOR && UNITY_EDITOR
 using Sirenix.OdinInspector;
@@ -20,138 +19,16 @@ namespace TinyMVC.Boot {
         #endif
         
         private DependencyContainer _container;
-        private int _targetEntityId;
         
         private readonly Dictionary<int, DependencyContainer> _dependencies;
-        private readonly Dictionary<int, List<Entity>> _entities;
-        private readonly Dictionary<Type, IData> _singleEntities;
         
-        public const int DEFAULT_GROUP = -1;
         private const int _CAPACITY = 16;
-        private const int _SINGLE_CAPACITY = 32;
         
         internal ProjectData() {
-            _targetEntityId = 0;
             _dependencies = new Dictionary<int, DependencyContainer>(_CAPACITY);
-            _entities = new Dictionary<int, List<Entity>>(_CAPACITY);
-            _singleEntities = new Dictionary<Type, IData>(_SINGLE_CAPACITY);
             #if ODIN_INSPECTOR && UNITY_EDITOR
             _dependenciesEditor = new List<DependencyContainer>(_CAPACITY);
             #endif
-        }
-        
-        public Entity AddEntity(int id = DEFAULT_GROUP) {
-            Entity entity = new Entity(_targetEntityId++, id);
-            
-            if (_entities.TryGetValue(id, out List<Entity> entities)) {
-                entities.Add(entity);
-            } else {
-                _entities.Add(id, new List<Entity>() { entity });
-            }
-            
-            return entity;
-        }
-        
-        public void RemoveEntity(Entity entity) {
-            if (_entities.TryGetValue(entity.groupId, out List<Entity> entities)) {
-                entities.Remove(entity);
-            }
-        }
-        
-        public void SetSingle<T>(T component) where T : IData => _singleEntities.Add(typeof(T), component);
-        
-        public void RemoveSingle<T>() where T : IData => _singleEntities.Remove(typeof(T));
-        
-        public bool TryGetSingle<T>(out T component) where T : IData {
-            if (_singleEntities.TryGetValue(typeof(T), out IData componentData)) {
-                component = (T)componentData;
-                
-                return true;
-            }
-            
-            component = default;
-            
-            return false;
-        }
-        
-        public IEnumerable<T> ForEach<T>() where T : IData {
-            foreach (List<Entity> entities in _entities.Values) {
-                foreach (Entity entity in entities) {
-                    foreach (T data in entity.ForEach<T>()) {
-                        yield return data;
-                    }
-                }
-            }
-        }
-        
-        public IEnumerable<(Entity, T)> ForEachEntities<T>() where T : IData {
-            foreach (List<Entity> entities in _entities.Values) {
-                foreach (Entity entity in entities) {
-                    if (entity.TryGetEntity(out(Entity, T) data)) {
-                        yield return data;
-                    }
-                }
-            }
-        }
-        
-        public IEnumerable<(Entity, T1, T2)> ForEachEntities<T1, T2>() where T1 : IData where T2 : IData {
-            foreach (List<Entity> entities in _entities.Values) {
-                foreach (Entity entity in entities) {
-                    if (entity.TryGetEntity(out(Entity, T1, T2) data)) {
-                        yield return data;
-                    }
-                }
-            }
-        }
-        
-        public IEnumerable<(Entity, T1, T2, T3)> ForEachEntities<T1, T2, T3>() where T1 : IData where T2 : IData where T3 : IData {
-            foreach (List<Entity> entities in _entities.Values) {
-                foreach (Entity entity in entities) {
-                    if (entity.TryGetEntity(out(Entity, T1, T2, T3) data)) {
-                        yield return data;
-                    }
-                }
-            }
-        }
-        
-        public IEnumerable<(Entity, T1, T2, T3, T4)> ForEachEntities<T1, T2, T3, T4>() where T1 : IData where T2 : IData where T3 : IData where T4 : IData {
-            foreach (List<Entity> entities in _entities.Values) {
-                foreach (Entity entity in entities) {
-                    if (entity.TryGetEntity(out(Entity, T1, T2, T3, T4) data)) {
-                        yield return data;
-                    }
-                }
-            }
-        }
-        
-        public IEnumerable<(T1, T2)> ForEach<T1, T2>() where T1 : IData where T2 : IData {
-            foreach (List<Entity> entities in _entities.Values) {
-                foreach (Entity entity in entities) {
-                    foreach ((T1, T2) data in entity.ForEach<T1, T2>()) {
-                        yield return data;
-                    }
-                }
-            }
-        }
-        
-        public IEnumerable<(T1, T2, T3)> ForEach<T1, T2, T3>() where T1 : IData where T2 : IData where T3 : IData {
-            foreach (List<Entity> entities in _entities.Values) {
-                foreach (Entity entity in entities) {
-                    foreach ((T1, T2, T3) data in entity.ForEach<T1, T2, T3>()) {
-                        yield return data;
-                    }
-                }
-            }
-        }
-        
-        public IEnumerable<(T1, T2, T3, T4)> ForEach<T1, T2, T3, T4>() where T1 : IData where T2 : IData where T3 : IData where T4 : IData {
-            foreach (List<Entity> entities in _entities.Values) {
-                foreach (Entity entity in entities) {
-                    foreach ((T1, T2, T3, T4) data in entity.ForEach<T1, T2, T3, T4>()) {
-                        yield return data;
-                    }
-                }
-            }
         }
         
         public void Resolve(List<IResolving> resolving) {
