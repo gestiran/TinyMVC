d41e20c1f01921344896ba00c9083270559a47b2
Derek Sliman <lionext.studio@gmail.com>
2024-06-28 19:43:32 +0300

    Add begin-play profiling

--- Scripts/Loop/Extensions/BeginPlayExtension.cs
+++ Scripts/Loop/Extensions/BeginPlayExtension.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
 using TinyMVC.Debugging;
 using TinyMVC.Debugging.Exceptions;
+using Unity.Profiling;
+using Unity.Profiling.LowLevel;
+using UnityEngine;
 #endif
 
 namespace TinyMVC.Loop.Extensions {
@@ -63,21 +66,55 @@ namespace TinyMVC.Loop.Extensions {
         }
         
         public static async Task TryBeginPlayAsync<T>(this List<T> objects) {
+            #if UNITY_EDITOR || DEVELOPMENT_BUILD
+            ProfilerMarker group = new ProfilerMarker(ProfilerCategory.Scripts, "TinyMVC.BeginPlay", MarkerFlags.Script);
+            
+            group.Begin();
+            #endif
+            
             for (int objId = 0; objId < objects.Count; objId++) {
                 if (objects[objId] is IBeginPlayAsync otherAsync) {
                     #if UNITY_EDITOR || DEVELOPMENT_BUILD
+                    
+                    ProfilerMarker profile = new ProfilerMarker(ProfilerCategory.Scripts, $"{otherAsync.GetType().Name}", MarkerFlags.Script);
+                    
+                    if (otherAsync is Object unityObject) {
+                        profile.Begin(unityObject);
+                    } else {
+                        profile.Begin();
+                    }
+                    
                     await DebugUtility.ReThrowAsync(() => otherAsync.BeginPlay(), exception => new BeginPlayAsyncException(otherAsync, exception));
+                    
+                    profile.End();
+                    
                     #else
                     await otherAsync.BeginPlay();
                     #endif
                 } else if (objects[objId] is IBeginPlay other) {
                     #if UNITY_EDITOR || DEVELOPMENT_BUILD
+                    
+                    ProfilerMarker profile = new ProfilerMarker(ProfilerCategory.Scripts, $"{other.GetType().Name}", MarkerFlags.Script);
+                    
+                    if (other is Object unityObject) {
+                        profile.Begin(unityObject);
+                    } else {
+                        profile.Begin();
+                    }
+                    
                     DebugUtility.ReThrow(() => other.BeginPlay(), exception => new BeginPlayException(other, exception));
+                    
+                    profile.End();
+                    
                     #else
                     other.BeginPlay();
                     #endif
                 }
             }
+            
+            #if UNITY_EDITOR || DEVELOPMENT_BUILD
+            group.End();
+            #endif
         }
         
         public static void BeginPlay<TBeginPlay>(this TBeginPlay[] objects) where TBeginPlay : IBeginPlay {
