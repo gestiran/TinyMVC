b7a1b125a4129c3a5bfd082436965b40cfe81316
Derek Sliman <lionext.studio@gmail.com>
2024-08-04 19:09:34 +0300

    Added log changes and increment

--- Scripts/ReactiveFields/Extensions/ObservedExtension.cs
+++ Scripts/ReactiveFields/Extensions/ObservedExtension.cs
@@ -1,11 +1,42 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
+using TinyMVC.Boot;
+using TinyMVC.Loop;
 using UnityEngine;
 
 namespace TinyMVC.ReactiveFields.Extensions {
     public static class ObservedExtension {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Increment(this Observed<int> observed) => observed.Set(observed.value + 1);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Increment(this Observed<float> observed) => observed.Set(observed.value + 1);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Decrement(this Observed<int> observed) => observed.Set(observed.value - 1);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Decrement(this Observed<float> observed) => observed.Set(observed.value - 1);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AddValue(this Observed<int> observed, int value) => observed.Set(observed.value + value);
+        
+        [Obsolete("Can't add nothing!", true)]
+        public static void AddValue(this Observed<int> observed) {
+            // Do nothing
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsActive(this Observed<int> observed) => observed.value >= 0;
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsCurrent(this Observed<int> observed, int current) => observed.value == current;
+        
+        [Obsolete("Can't add nothing!", true)]
+        public static void AddValue(this Observed<float> observed) {
+            // Do nothing
+        }
         
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AddValueMax(this Observed<int> observed, int value, int maxValue) => observed.Set(Mathf.Min(observed.value + value, maxValue));
@@ -130,6 +161,51 @@ namespace TinyMVC.ReactiveFields.Extensions {
             observed.Set(value);
             
             return true;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Toggle(this Observed<bool> observed) {
+            if (observed.value) {
+                observed.Set(false);
+            } else {
+                observed.Set(true);
+            }
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogChanges<T>(this Observed<T> observed, string label) {
+            ProjectContext.current.TryGetGlobalUnload(out UnloadPool unload);
+            
+            observed.AddListener(value => Debug.Log($"{label}: {value}"), unload);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogChanges<T>(this Observed<T> observed, string label, UnloadPool unload) {
+            observed.AddListener(value => Debug.Log($"{label}: {value}"), unload);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogWarningChanges<T>(this Observed<T> observed, string label) {
+            ProjectContext.current.TryGetGlobalUnload(out UnloadPool unload);
+            
+            observed.AddListener(value => Debug.LogWarning($"{label}: {value}"), unload);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogWarningChanges<T>(this Observed<T> observed, string label, UnloadPool unload) {
+            observed.AddListener(value => Debug.LogWarning($"{label}: {value}"), unload);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogErrorChanges<T>(this Observed<T> observed, string label) {
+            ProjectContext.current.TryGetGlobalUnload(out UnloadPool unload);
+            
+            observed.AddListener(value => Debug.LogError($"{label}: {value}"), unload);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LogErrorChanges<T>(this Observed<T> observed, string label, UnloadPool unload) {
+            observed.AddListener(value => Debug.LogError($"{label}: {value}"), unload);
         }
     }
 }
