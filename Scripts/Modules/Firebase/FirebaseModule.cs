cd8c9f709b88d16dcd5eddc4d568e660cce2a915
Derek Sliman <lionext.studio@gmail.com>
2024-08-04 19:12:36 +0300

    Firebase async initialization

--- Scripts/Modules/Firebase/FirebaseModule.cs
+++ Scripts/Modules/Firebase/FirebaseModule.cs
@@ -1,36 +1,16 @@
 ﻿#if GOOGLE_FIREBASE_APP
 using System.Threading.Tasks;
 using Firebase;
-using Firebase.Extensions;
 using UnityEngine;
 
 namespace TinyMVC.Modules.Firebase {
     public sealed class FirebaseModule : IApplicationModule {
         public DependencyStatus status { get; private set; }
         
-        private const int _MAX_DELAY = 3000;
-        private const int _STEP = 100;
+        public FirebaseModule() => status = DependencyStatus.UnavailableDisabled;
         
-        public FirebaseModule() {
-            status = DependencyStatus.UnavailableDisabled;
-            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(OnFixedDependencies);
-        }
-        
-        public async Task WaitInitialization() {
-            int time = _MAX_DELAY;
-            
-            while (time > 0) {
-                if (status == DependencyStatus.Available) {
-                    return;
-                }
-                
-                await Task.Delay(_STEP);
-                time -= _STEP;
-            }
-        }
-        
-        private void OnFixedDependencies(Task<DependencyStatus> result) {
-            status = result.Result;
+        public async Task Initialize() {
+            status = await FirebaseApp.CheckAndFixDependenciesAsync();
             
             if (status == DependencyStatus.Available) {
                 FirebaseApp _ = FirebaseApp.DefaultInstance;
@@ -38,6 +18,8 @@ namespace TinyMVC.Modules.Firebase {
             } else {
                 Debug.LogError($"FirebaseModule: {status}");
             }
+            
+            await Task.Yield();
         }
     }
 }
