1e1c646bee798797ba9c4537434e51adeb30277d
gestiran <lionext.studio@gmail.com>
2024-12-11 13:07:41 +0200

    Added abstract bool binder

---
+++ Scripts/Boot/Binding/DependencyAbstractPoolBinder.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Runtime.CompilerServices;
+using TinyMVC.Dependencies;
+using TinyMVC.Loop;
+
+namespace TinyMVC.Boot.Binding {
+    public abstract class DependencyAbstractPoolBinder<T> : Binder where T : IDependency {
+        protected abstract int _count { get; }
+        
+        protected DependencyAbstractPoolBinder(string key = null) => keyValue = key;
+        
+        public override IDependency GetDependency() => Bind();
+        
+        internal override Type GetBindType() => typeof(DependencyPool<T>);
+        
+        public DependencyPool<T> Bind() {
+            if (this is IInit init) {
+                init.Init();
+            }
+            
+            DependencyPool<T> models = new DependencyPool<T>(_count);
+            FillModels(models);
+            Bind(models);
+            return models;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected void FillModels(DependencyPool<T> models) {
+            for (int modelId = 0; modelId < models.length; modelId++) {
+                models[modelId] = New(modelId);
+            }
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected virtual void Bind(DependencyPool<T> models) {
+            for (int modelId = 0; modelId < models.length; modelId++) {
+                Bind(models[modelId], modelId);
+            }
+        }
+        
+        protected abstract void Bind(T model, int index);
+        
+        protected abstract T New(int index);
+    }
+}
