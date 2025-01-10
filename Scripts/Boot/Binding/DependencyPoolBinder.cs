c1cb9a7ac023bee2fba2d4bd3dddb540874ff61d
Derek Sliman <lionext.studio@gmail.com>
2025-01-10 18:45:07 +0200

    Fixed key initialization

--- Scripts/Boot/Binding/DependencyPoolBinder.cs
+++ Scripts/Boot/Binding/DependencyPoolBinder.cs
@@ -2,6 +2,8 @@
 
 namespace TinyMVC.Boot.Binding {
     public abstract class DependencyPoolBinder<T> : DependencyAbstractPoolBinder<T> where T : IDependency, new() {
+        protected DependencyPoolBinder(string key = null) => keyValue = key;
+        
         protected override T New(int index) => new T();
     }
 }
