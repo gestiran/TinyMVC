dea3a9b6ea9b6c9050c94f72e46155270667f356
Derek Sliman <lionext.studio@gmail.com>
2025-05-02 17:17:21 +0300

    Opened UnloadAction

--- Scripts/Loop/UnloadAction.cs
+++ Scripts/Loop/UnloadAction.cs
@@ -4,9 +4,7 @@ namespace TinyMVC.Loop {
     public sealed class UnloadAction : IUnload {
         private readonly Action _action;
         
-        internal UnloadAction(Action action) => _action = action;
-        
-        public static implicit operator UnloadAction(Action action) => new UnloadAction(action);
+        public UnloadAction(Action action) => _action = action;
         
         public void Unload() => _action.Invoke();
     }
