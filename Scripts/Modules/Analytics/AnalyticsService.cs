2f10204a4447d085d1851e8dfde262b7f4bd97bd
Derek Sliman <lionext.studio@gmail.com>
2025-05-04 11:56:27 +0300

    Organize firebase and analytics

--- Scripts/Modules/Analytics/AnalyticsService.cs
+++ Scripts/Modules/Analytics/AnalyticsService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -11,69 +10,69 @@ using TinyMVC.Modules.Firebase;
 #endif
 
 namespace TinyMVC.Modules.Analytics {
-    public sealed class AnalyticsModule : IApplicationModule {
-        private readonly AnalyticsLog _log;
+    public static class AnalyticsService {
+        private static readonly AnalyticsLog _log;
         
-        public AnalyticsModule() => _log = new AnalyticsLog();
+        static AnalyticsService() => _log = new AnalyticsLog();
         
-        public void ApplyConsent() {
+        public static void ApplyConsent() {
         #if GOOGLE_FIREBASE_ANALYTICS
-            if (API<FirebaseModule>.module.status != DependencyStatus.Available) {
-                Debug.Log("AnalyticsModule: Apply consent failed, firebase not available!");
+            if (FirebaseService.status != DependencyStatus.Available) {
+                Debug.Log("AnalyticsService: Apply consent failed, firebase not available!");
                 return;
             }
             
             FirebaseAnalytics.SetConsent(GeneratePermissions(ConsentStatus.Granted));
             FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
-            Debug.Log("AnalyticsModule: Apply consent success!");
+            Debug.Log("AnalyticsService: Apply consent success!");
         #else
-            Debug.Log("AnalyticsModule: Apply consent failed, module is inactive!");
+            Debug.Log("AnalyticsService: Apply consent failed, module is inactive!");
         #endif
         }
         
-        public void RejectConsent() {
+        public static void RejectConsent() {
         #if GOOGLE_FIREBASE_ANALYTICS
-            if (API<FirebaseModule>.module.status != DependencyStatus.Available) {
-                Debug.Log("AnalyticsModule: Reject consent failed, firebase not available!");
+            if (FirebaseService.status != DependencyStatus.Available) {
+                Debug.Log("AnalyticsService: Reject consent failed, firebase not available!");
                 return;
             }
             
             FirebaseAnalytics.SetConsent(GeneratePermissions(ConsentStatus.Denied));
             FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
-            Debug.Log("AnalyticsModule: Reject consent success!");
+            Debug.Log("AnalyticsService: Reject consent success!");
         #else
-            Debug.Log("AnalyticsModule: Reject consent failed, module is inactive!");
+            Debug.Log("AnalyticsService: Reject consent failed, module is inactive!");
         #endif
         }
         
         [Obsolete("Can't send empty event!", true)]
-        public void LogEvent() => Debug.LogError("Don't have parameters!");
+        public static void LogEvent() => Debug.LogError("Don't have parameters!");
         
-        public void LogEvent(string eventName) => SendEvent(new AnalyticsEvent(eventName));
+        public static void LogEvent(string eventName) => SendEvent(new AnalyticsEvent(eventName));
         
-        public void LogEvent(string eventName, params AnalyticsParameter[] parameters) => SendEvent(new AnalyticsEvent(eventName, parameters));
+        public static void LogEvent(string eventName, params AnalyticsParameter[] parameters) => SendEvent(new AnalyticsEvent(eventName, parameters));
         
-        public void LogEvent(string eventName, string parameterName, string parameterValue) {
+        public static void LogEvent(string eventName, string parameterName, string parameterValue) {
             SendEvent(new AnalyticsEvent(eventName, new AnalyticsParameter(parameterName, parameterValue)));
         }
         
-        public void LogEvent(string eventName, string parameterName, long parameterValue) {
+        public static void LogEvent(string eventName, string parameterName, long parameterValue) {
             SendEvent(new AnalyticsEvent(eventName, new AnalyticsParameter(parameterName, parameterValue)));
         }
         
-        public void LogEvent(string eventName, string parameterName, double parameterValue) {
+        public static void LogEvent(string eventName, string parameterName, double parameterValue) {
             SendEvent(new AnalyticsEvent(eventName, new AnalyticsParameter(parameterName, parameterValue)));
         }
         
-        public void LogEvent(AnalyticsEvent data) => SendEvent(data);
+        public static void LogEvent(AnalyticsEvent data) => SendEvent(data);
         
-        public void LogEvent([NotNull] params AnalyticsEvent[] data) {
+        public static void LogEvent([NotNull] params AnalyticsEvent[] data) {
             for (int i = 0; i < data.Length; i++) {
                 SendEvent(data[i]);
             }
         }
         
-        private void SendEvent(AnalyticsEvent data) {
+        private static void SendEvent(AnalyticsEvent data) {
         #if GOOGLE_FIREBASE_ANALYTICS
             switch (data.eventType) {
                 case AnalyticsEvent.EventType.EventOnly: FirebaseAnalytics.LogEvent(data.eventName); break;
@@ -97,8 +96,7 @@ namespace TinyMVC.Modules.Analytics {
         }
         
     #if GOOGLE_FIREBASE_ANALYTICS
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private Dictionary<ConsentType, ConsentStatus> GeneratePermissions(ConsentStatus state) {
+        private static Dictionary<ConsentType, ConsentStatus> GeneratePermissions(ConsentStatus state) {
             Dictionary<ConsentType, ConsentStatus> consent = new Dictionary<ConsentType, ConsentStatus>();
             
             consent.Add(ConsentType.AnalyticsStorage, state);
---
+++ Scripts/Modules/Firebase/FirebaseService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+#if GOOGLE_FIREBASE_APP
+using Firebase;
+#endif
+
+namespace TinyMVC.Modules.Firebase {
+    public static class FirebaseService {
+    #if GOOGLE_FIREBASE_APP
+        public static DependencyStatus status { get; private set; }
+        
+        static FirebaseService() => status = DependencyStatus.UnavailableDisabled;
+        
+        public static async Task Initialize() {
+            status = await FirebaseApp.CheckAndFixDependenciesAsync();
+            
+            if (status == DependencyStatus.Available) {
+                FirebaseApp _ = FirebaseApp.DefaultInstance;
+                Debug.Log($"FirebaseService: {status}");
+            } else {
+                Debug.LogError($"FirebaseService: {status}");
+            }
+            
+            await Task.Yield();
+        }
+    #endif
+    }
+}
