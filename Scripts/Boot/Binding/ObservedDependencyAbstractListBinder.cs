eddcd165b199adbe6d4fd614b5f1b51a1e554cc0
Derek Sliman <lionext.studio@gmail.com>
2025-05-14 01:23:10 +0300

    Added abstract list binder

---
+++ Scripts/Boot/Binding/ObservedDependencyAbstractListBinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TinyMVC.Dependencies;
+using TinyMVC.Loop;
+
+namespace TinyMVC.Boot.Binding {
+    public abstract class ObservedDependencyAbstractListBinder<T> : Binder where T : IDependency {
+        protected virtual int _capacity { get; } = 4;
+        
+        protected ObservedDependencyAbstractListBinder(string key = null) => keyValue = key;
+        
+        public override IDependency GetDependency() => Bind();
+        
+        internal override Type GetBindType() => typeof(ObservedDependencyList<T>);
+        
+        public ObservedDependencyList<T> Bind() {
+            if (this is IInit init) {
+                init.Init();
+            }
+            
+            ObservedDependencyList<T> model = new ObservedDependencyList<T>(_capacity);
+            Bind(model);
+            
+            return model;
+        }
+        
+        protected void AddCount(ObservedDependencyList<T> models, int count, Action<T, int> bind) {
+            for (int i = 0; i < count; i++) {
+                T model = New();
+                bind(model, i);
+                models.Add(model);
+            }
+        }
+        
+        protected abstract void Bind(ObservedDependencyList<T> model);
+        
+        protected abstract T New();
+    }
+}
