e4dbe5357149edec9542b4c67f9b4993d0ef251a
Derek Sliman <lionext.studio@gmail.com>
2025-05-29 14:30:54 +0300

    Added tokens counter

---
+++ Scripts/Modules/ADS/ADSTokensCount.cs
@@ -0,0 +1,48 @@
+﻿using Sirenix.OdinInspector;
+
+namespace TinyMVC.Modules.ADS {
+    [ShowInInspector, InlineProperty, HideDuplicateReferenceBox, HideReferenceObjectPicker]
+    public sealed class ADSTokensCount {
+        [field: ShowInInspector, OnValueChanged("ApplyCount"), HorizontalGroup, HideLabel]
+        public int count { get; private set; }
+        
+        public ADSTokensCount() {
+            count = API<ADSTokenModule>.module.tokenCount;
+            API<ADSTokenModule>.module.onCountChanged += UpdateCount;
+        }
+        
+        ~ADSTokensCount() {
+            API<ADSTokenModule>.module.onCountChanged -= UpdateCount;
+        }
+        
+    #if UNITY_EDITOR
+        private void ApplyCount() {
+            int current = API<ADSTokenModule>.module.tokenCount;
+            
+            if (count == current) {
+                return;
+            }
+            
+            if (count > current) {
+                API<ADSTokenModule>.module.AddTokens(count - current);
+            } else {
+                API<ADSTokenModule>.module.SubtractTokens(current - count);
+            }
+        }
+        
+        private void UpdateCount(int value) => count = value;
+        
+        [Button("+1"), HorizontalGroup]
+        private void Add() {
+            count++;
+            ApplyCount();
+        }
+        
+        [Button("-1"), HorizontalGroup]
+        private void Subtract() {
+            count--;
+            ApplyCount();
+        }
+    #endif
+    }
+}
