7e44be61f7c202aadf8e0c7173ebd1b654ba3221
Derek Sliman <lionext.studio@gmail.com>
2025-06-17 21:02:51 +0300

    Organized actor type and display

--- Scripts/Dependencies/Components/Actor.cs
+++ Scripts/Dependencies/Components/Actor.cs
@@ -1,11 +1,37 @@
+using Sirenix.OdinInspector;
 using TinyMVC.Views;
 
 namespace TinyMVC.Dependencies.Components {
     public abstract class Actor<T> : Actor where T : View {
-        public new T view { get; internal set; }
+    #if UNITY_EDITOR
+        internal override bool isVisibleBaseView => false;
+    #endif
+        
+        [ShowInInspector, ReadOnly]
+        public new T view {
+            get => viewTypeInternal;
+            internal set => viewTypeInternal = value;
+        }
+        
+        internal override View viewInternal { get => viewTypeInternal; set => viewTypeInternal = value as T; }
+        
+        [ShowInInspector, LabelText("View"), ReadOnly]
+        internal T viewTypeInternal { get; set; }
     }
     
     public abstract class Actor : Model {
-        public View view { get; internal set; }
+    #if UNITY_EDITOR
+        
+        internal virtual bool isVisibleBaseView => true;
+        
+    #endif
+        
+        public View view {
+            get => viewInternal;
+            internal set => viewInternal = value;
+        }
+        
+        [ShowInInspector, LabelText("View"), ShowIf("isVisibleBaseView"), ReadOnly]
+        internal virtual View viewInternal { get; set; }
     }
 }
