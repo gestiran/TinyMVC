5b6deab18ebc398c0c4ba154b0d272df48c4b45e
Derek Sliman <lionext.studio@gmail.com>
2025-07-12 00:21:18 +0300

    Added refresh pool buttons

--- Scripts/Views/ViewGlobalPool.cs
+++ Scripts/Views/ViewGlobalPool.cs
@@ -1,25 +1,46 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Sirenix.OdinInspector;
 
 namespace TinyMVC.Views {
-    public abstract class ViewGlobalPool<T> : View where T : View {
+    public abstract class ViewPoolGlobal<T> : View, IEnumerable<T> where T : View {
         public int length => views.Length;
         
-        [field: SerializeField, LabelWidth(25), ListDrawerSettings(ShowIndexLabels = true, DraggableItems = false, HideRemoveButton = true), OnValueChanged("OnViewsChanged")]
+        [field: SerializeField, LabelWidth(25), OnValueChanged("OnViewsChanged")]
+        [field: ListDrawerSettings(ShowIndexLabels = true, DraggableItems = false, HideRemoveButton = true, OnTitleBarGUI = "ListGUI")]
         public T[] views { get; private set; }
         
         public T this[int index] => views[index];
         
+        public IEnumerator<T> GetEnumerator() {
+            for (int viewId = 0; viewId < views.Length; viewId++) {
+                yield return views[viewId];
+            }
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
     #if UNITY_EDITOR
         
         public override void Reset() {
-            ApplyViews_Editor(FindObjectsOfType<T>(true));
+            UpdateViews();
             base.Reset();
         }
         
         protected virtual void ApplyViews_Editor(T[] value) => views = value;
         
         protected virtual void OnViewsChanged() { }
+        
+        // ReSharper disable once UnusedMember.Local
+        private void ListGUI() {
+            if (Sirenix.Utilities.Editor.SirenixEditorGUI.ToolbarButton(Sirenix.Utilities.Editor.EditorIcons.Refresh)) {
+                UpdateViews();
+                UnityEditor.EditorUtility.SetDirty(this);
+            }
+        }
+        
+        private void UpdateViews() => ApplyViews_Editor(FindObjectsOfType<T>(true));
     
     #endif
     }
--- Scripts/Views/ViewPool.cs
+++ Scripts/Views/ViewPool.cs
@@ -1,23 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Sirenix.OdinInspector;
 
 namespace TinyMVC.Views {
-    public abstract class ViewPool<T> : View {
+    public abstract class ViewPool<T> : View, IEnumerable<T> {
         public int length => views.Length;
         
-        [field: SerializeField, OnValueChanged("OnViewsChanged")]
+        [field: SerializeField, LabelWidth(25), OnValueChanged("OnViewsChanged")]
+        [field: ListDrawerSettings(ShowIndexLabels = true, DraggableItems = false, HideRemoveButton = true, OnTitleBarGUI = "ListGUI")]
         public T[] views { get; private set; }
         
         public T this[int index] => views[index];
         
+        public IEnumerator<T> GetEnumerator() {
+            for (int viewId = 0; viewId < views.Length; viewId++) {
+                yield return views[viewId];
+            }
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
     #if UNITY_EDITOR
         
         public override void Reset() {
-            views = GetComponentsInChildren<T>(true);
+            UpdateViews();
             base.Reset();
         }
         
         protected virtual void OnViewsChanged() { }
+        
+        // ReSharper disable once UnusedMember.Local
+        private void ListGUI() {
+            if (Sirenix.Utilities.Editor.SirenixEditorGUI.ToolbarButton(Sirenix.Utilities.Editor.EditorIcons.Refresh)) {
+                UpdateViews();
+                UnityEditor.EditorUtility.SetDirty(this);
+            }
+        }
+        
+        private void UpdateViews() => views = GetComponentsInChildren<T>(true);
         
     #endif
     }
