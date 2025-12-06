4429209aee8b5443470bb9487a483923142dc7a0
Derek Sliman <lionext.studio@gmail.com>
2025-12-06 16:23:51 +0200

    Added spawn controller

---
+++ Scripts/Controllers/SpawnController.cs
@@ -0,0 +1,25 @@
+// Copyright (c) 2023 Derek Sliman
+// Licensed under the MIT License. See LICENSE.md for details.
+
+using JetBrains.Annotations;
+using TinyMVC.Boot;
+using TinyMVC.Views;
+
+namespace TinyMVC.Controllers {
+    public abstract class SpawnController : IController {
+        protected T Spawn<T>([NotNull] T view) where T : View => Spawn(view, ProjectContext.activeContext.key);
+        
+        protected T Spawn<T>([NotNull] T view, string contextKey) where T : View {
+            if (view.connectState == View.ConnectState.Connected) {
+                return view;
+            }
+            
+            if (ProjectContext.TryGetContext(contextKey, out SceneContext context)) {
+                view.connectState = View.ConnectState.Connected;
+                context.Connect(view);
+            }
+            
+            return view;
+        }
+    }
+}
