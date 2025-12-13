8c17c4c05bfb2a0a1bcd0736c8ef09d174f813ab
Derek Sliman <lionext.studio@gmail.com>
2025-12-13 21:11:32 +0200

    Added custom save variation

--- Scripts/Modules/Saving/Reactive/ObservedSave.cs
+++ Scripts/Modules/Saving/Reactive/ObservedSave.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2023 Derek Sliman
 // Licensed under the MIT License. See LICENSE.md for details.
 
+using System;
 using TinyReactive.Fields;
 
 namespace TinyMVC.Modules.Saving.Reactive {
@@ -32,5 +33,45 @@ namespace TinyMVC.Modules.Saving.Reactive {
         }
         
         private static void EmptySave(T newValue) { }
+    }
+    
+    public class ObservedSave<T1, T2> : Observed<T1> {
+        private ActionListener<T1> _save;
+        
+        public ObservedSave(Func<T1, T2> to, Func<T2, T1> from, string key) : this(to, from, default(T1), key) { }
+        
+        public ObservedSave(Func<T1, T2> to, Func<T2, T1> from, T1 defaultValue, string key) : base(defaultValue) {
+            if (SaveService.Has(key)) {
+                _value = from.Invoke(SaveService.Load(default(T2), key));
+            } else {
+                _value = defaultValue;
+            }
+            
+            _save = newValue => SaveService.Save(to(newValue), key);
+        }
+        
+        public ObservedSave(Func<T1, T2> to, Func<T2, T1> from, string key, params string[] group) : this(to, from, default, key, group) { }
+        
+        public ObservedSave(Func<T1, T2> to, Func<T2, T1> from, T1 defaultValue, string key, params string[] group) : base(defaultValue) {
+            if (SaveService.Has(key, group)) {
+                _value = from.Invoke(SaveService.Load(default(T2), key, group));
+            } else {
+                _value = defaultValue;
+            }
+            
+            _save = newValue => SaveService.Save(to.Invoke(newValue), key, group);
+        }
+        
+        public override void Set(T1 newValue) {
+            base.Set(newValue);
+            _save.Invoke(newValue);
+        }
+        
+        public override void Unload() {
+            base.Unload();
+            _save = EmptySave;
+        }
+        
+        private static void EmptySave(T1 newValue) { }
     }
 }
